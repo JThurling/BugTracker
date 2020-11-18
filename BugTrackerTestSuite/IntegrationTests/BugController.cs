b3c0b115c0bfe5c69c940635537ba21646c8e70d
Jacques Thurling <jthurling78@gmail.com>
2020-11-18 17:49:21 +0200

    Added Extra Tests

--- BugTrackerTestSuite/IntegrationTests/BugController.cs
+++ BugTrackerTestSuite/IntegrationTests/BugController.cs
@@ -39,5 +39,38 @@ namespace BugTrackerTestSuite.IntegrationTests
             response.StatusCode.Should().Be(HttpStatusCode.OK);
             Assert.Contains("Hello", responseString);
         }
+
+        [Fact]
+        public async Task GetBug_WhenCalled_GetsListOfBugs()
+        {
+            var response = await _client.GetAsync("api/Bug");
+            response.EnsureSuccessStatusCode();
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task GetBug_WhenCalled_GetSingleBug()
+        {
+            var response = await _client.GetAsync("api/Bug");
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task DeleteBug_WhenCalled_DeletesBug()
+        {
+            var response = await _client.PostAsync("api/Bug", new StringContent(JsonConvert.SerializeObject(new BugInput
+            {
+                Bug = "Hello",
+                Deadline = DateTime.Today,
+                Priority = Priority.Low,
+                Status = Status.Open
+            }), Encoding.UTF8, "application/json"));
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            Assert.Contains("Hello", responseString);
+        }
     }
 }
--- Server/Controllers/BugController.cs
+++ Server/Controllers/BugController.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Core.Models.Inputs.Bug;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,6 +9,24 @@ namespace BugTracker.Server.Controllers
     {
         [HttpPost]
         public async Task<ActionResult<BugInput>> CreateBug([FromBody]BugInput bug)
+        {
+            return Ok(bug);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<BugInput>>> GetBugs([FromBody] BugInput bugs)
+        {
+            return Ok(bugs);
+        }
+
+        [HttpGet("/{id}")]
+        public async Task<ActionResult<BugInput>> GetSingleBug([FromBody] BugInput bugs)
+        {
+            return Ok(bugs);
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<BugInput>> DeleteBug([FromBody] BugInput bug)
         {
             return Ok(bug);
         }
